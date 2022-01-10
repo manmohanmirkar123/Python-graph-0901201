83780aa44c008f5d70d346a0866b7e259c0d1c28
manmohan-at-450193739025 <manmohanmirkar@Manmohans-MacBook-Air.local>
2022-01-10 10:30:14 +0530

    Initial commit

---
+++ drawing.py
@@ -0,0 +1,13 @@
+import turtle
+t = turtle.Turtle()
+mylist = ["purple","red","orange","blue","green"]
+#print("By Manmohan Mirkar")
+#turtle.write('By Manmohan Mirkar!')
+turtle.color('black')
+Style = ('Courier', 100, 'italic')
+turtle.write('Manmohan      Mirkar!!', font=Style, align='center')
+for i in range(200):
+    t.color(mylist[i%5])
+    t.pensize(i/10+1)
+    t.forward(i)
+    t.left(75)
